2dba9f2a0eb890d917098d959ecc7ef1efed0809
chen.lili <chen.lili@jcbc.co.jp>
2024-10-17 20:33:06 +0900

    modify the startup script

--- script/unknown.sh
+++ script/unknown.sh
@@ -1,13 +1,2 @@
 #!/bin/bash
-# Please add this script to "Startup Applications" and use bash to execute it.
-# Please make sure that the path is correct.
-cd ~/Workspace/dalle-playground/backend/;
-# Please make sure that the virtual environment does exist.
-source dalle_venv/bin/activate;
-python3 app.py --port 8080&
-
-sleep 10 # wait for dall-e service completing start up
-
-# Please make sure that the path is correct.
-cd ~/Workspace/unknown-local-server/;
-./gradlew run&
+gradle run&
